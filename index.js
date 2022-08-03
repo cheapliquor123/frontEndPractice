e90214bda8cf3b614a32088cea5b5fd98109eea8
cheapliquor123 <82467194+cheapliquor123@users.noreply.github.com>
2022-08-02 20:26:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// let name = {
+//     name : 'Maddy',
+//     age : 26
+// };
+
+
+// //functions 
+
+// // document.getElementById("yourName").innerHTML = "Enter Your Full Name"  
+
+// let myAge = 26;
+
+// console.log(myAge)
+
+// ------------------------------------------------------------
+
+// let myAge = 26;
+// let humanDogRatio = 7;
+// let dogYears = myAge * humanDogRatio;
+
+// console.log(dogYears + " years")
+
+//---------------------------------------------------------------
+// let lap1 = 32;
+// let lap2 = 40;
+// let lap3 = 33;
+
+// function sumOfRaces() {
+//     let totalTime = lap1 + lap2 +lap3
+//     console.log(totalTime)
+// }
+
+// sumOfRaces()
+
+let lapsCompleted = 0;
+
+function incrementOfLaps() {
+    lapsCompleted = lapsCompleted +1;
+}
+
+incrementOfLaps()
+incrementOfLaps()
+incrementOfLaps()
+
+console.log(lapsCompleted)
